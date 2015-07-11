042b1d1796bafe2811b9b894fef4830e6f7d4241
RyanBacka <RKBacka@Fullsail.edu>
2015-07-11 16:08:43 -0400

    updated geo.js, network.js, and win.js Main code is written for pulling data from weather underground API. Code to find users latitude and longitude is written. fuctions to fire geo.js and network.js are not firing.

--- AVF1507 Project1/Resources/win.js
+++ AVF1507 Project1/Resources/win.js
@@ -3,19 +3,64 @@ var mainWin = Ti.UI.createWindow({
 	/*layout: vertical*/
 });
 
-var buildUi = function(lat, lng){
+var title = Ti.UI.createLabel({
+	text: "Weather App",
+	top: 20
+});
+
+var geo = require("geo");
+var net = require("network");
+
+var buildGeoUi = function(userLoc){
 	console.log(lat, lng);
 	mainWin.add(Ti.UI.createLabel({
-		text:"Latitude: " + lat,
-		top:30
+		text:"Latitude: " + userLoc.lat,
+		top:35
 	}));
 	mainWin.add(Ti.UI.createLabel({
-		text:"Longitude: " + lng,
+		text:"Longitude: " + userLoc.lng,
 		top:15
 	}));
-	
+	mainWin.add(latLabel, lngLabel);
 };
 
+var buildUi = function(weatherData){
+	mainWin.add(Ti.UI.createLabel({
+		text: net.forecast.simpleforecast.forecastday.high.fahrenheit,
+		top: 20,
+		left: 20
+	}));
+	mainWin.add(Ti.UI.createLabel({
+		text: net.forecast.simpleforecast.forecastday.low.fahrenheit,
+		top: 20,
+		left: 60
+	}));
+	mainWin.add(Ti.UI.createLabel({
+		text: net.location.city,
+		top: 40,
+		left: 20
+	}));
+	mainWin.add(Ti.UI.createLabel({
+		text: net.location.state,
+		top:40,
+		left: 60
+	}));
+	mainWin.add(Ti.UI.createLabel({
+		text: net.forecast.simpleforecast.forecastday.date.pretty,
+		top: 60,
+		left: 20
+	}));
+	mainWin.add(Ti.UI.createLabel({
+		text: net.forecast.simpleforecast.forecastday.conditions,
+		top: 60,
+		left: 20
+	}));
+	mainWin.add(Ti.UI.createImageView({
+		
+	}));
+};
+mainWin.add(title);
 mainWin.open();
 
-exports.mainWin = mainWin;
+exports.buildUi = buildUi;
+exports.buildGeoUi = buildGeoUi;
